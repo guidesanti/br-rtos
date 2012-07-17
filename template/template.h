f2eef854dd84e3488f66895ab36ad6e9dbec159c
guilherme.desanti <guilherme.desanti@gmail.com>
2012-07-17 23:41:54 +0000

    Added initial folder structure, header file template and source code file template.

---
+++ template/template.h
@@ -0,0 +1,68 @@
+/**
+ * @file        template.h
+ * @date        TODO
+ * @author      Guilherme A. de Santi <guidesanti@yahoo.com.br>
+ * @copyright   Guilherme A. de Santi &copy; 2012
+ * @brief       TODO: add brief description
+ *
+ * TODO: add detailed description
+ */
+
+
+#ifndef _TEMPLATE_H_
+#define _TEMPLATE_H_
+
+/**
+ * @defgroup ModuleName The Module Name
+ * @{
+ */
+
+/******************************************************************************/
+/* I N C L U D E S                                                            */
+/******************************************************************************/
+
+/**
+ * @defgroup Includes Header File Include
+ * @{
+ */
+
+/**@}*/
+
+/******************************************************************************/
+/* C O N S T A N T ,  M A C R O  A N D  T Y P E  D E F I N I T I O N S       */
+/******************************************************************************/
+
+/**
+ * @defgroup Defs Constant, Macro and Type Definitions
+ * @{
+ */
+
+/**@}*/
+
+/******************************************************************************/
+/* P U B L I C  V A R I A B L E S                                             */
+/******************************************************************************/
+
+/**
+ * @defgroup PublicVar Public Variables
+ * @{
+ */
+
+/**@}*/
+
+
+/******************************************************************************/
+/* P U B L I C  F U N C T I O N S                                             */
+/******************************************************************************/
+
+/**
+ * @defgroup PublicFunc Public Functions
+ * @{
+ */
+
+/**@}*/
+
+/**@}*/
+
+
+#endif /* _TEMPLATE_H_ */

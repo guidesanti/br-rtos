01fd62e64c4b5fbd9420d053dd84d375f119a060
guilherme.desanti <guilherme.desanti@gmail.com>
2012-07-29 14:58:44 +0000

    Changed the template files.

--- template/template.c
+++ template/template.c
@@ -1,87 +1,83 @@
 /**
- * @file        template.c
+ * @file        template.h
  * @date        TODO
  * @author      Guilherme A. de Santi <guidesanti@yahoo.com.br>
- * @copyright   Guilherme A. de Santi &copy; 2012
- * @brief       TODO: Add brief description
+ * @copyright   &copy; 2012, BR-RTOS Development Team
+ * @brief       TODO: add brief description
  *
- * TODO: Add detailed description
+ * This file is part of the BR-RTOS.
+ * The license distribution terms may found at COPYING or LICENSE file
+ * within the source directory tree.
  */
 
+/**
+ * @addtogroup ModuleName
+ * @{
+ */
 
 /******************************************************************************/
 /* I N C L U D E S                                                            */
 /******************************************************************************/
 
+/** @{ */
+
 /**
- * @defgroup Includes Header File Include
+ * Includes.
+ */
+
+/** @} */
+
+
+/******************************************************************************/
+/* C O N S T A N T ,  M A C R O  A N D  T Y P E  D E F I N I T I O N S        */
+/******************************************************************************/
+
+/**
+ * @defgroup ModuleNameDefinitions Module Definitions
  * @{
  */
 
-/**@}*/
+/** @} */
 
-/******************************************************************************/
-/* C O N S T A N T ,  M A C R O  A N D  T Y P E  D E F I N I T I O N S       */
-/******************************************************************************/
-
-/**
- * @defgroup Defs Constant, Macro and Type Definitions
- * @{
- */
-
-/**@}*/
 
 /******************************************************************************/
 /* P R I V A T E  F U N C T I O N S  P R O T O T Y P E S                      */
 /******************************************************************************/
 
-/**
- * @defgroup PrivateFuncProto Private Function Prototypes
- * @{
- */
-
-/**@}*/
 
 /******************************************************************************/
-/* P R I V A T E  V A R I A B L E S                                           */
+/* K E R N E L  I N T E R N A L  V A R I A B L E S                            */
 /******************************************************************************/
 
 /**
- * @defgroup PrivateVar Private Variables
+ * @defgroup ModuleNameKernelIntVar Kernel Internal Variables
  * @{
  */
 
-/**@}*/
+/** @} */
+
 
 /******************************************************************************/
-/* P U B L I C  V A R I A B L E S                                             */
+/* K E R N E L  I N T E R N A L  F U N C T I O N S                            */
 /******************************************************************************/
 
 /**
- * @defgroup PublicVar Public Variables
+ * @defgroup ModuleNameKernelIntFunc Kernel Internal Functions
  * @{
  */
 
-/**@}*/
+/** @} */
 
 /******************************************************************************/
-/* P R I V A T E  F U N C T I O N S                                           */
+/* P U B L I C  A P I  F U N C T I O N S                                      */
 /******************************************************************************/
 
 /**
- * @defgroup PrivateFunc Private Functions
+ * @defgroup ModuleNamePublicAPIFunc Public API Functions
  * @{
  */
 
-/**@}*/
+/** @} */
 
-/******************************************************************************/
-/* P U B L I C  F U N C T I O N S                                             */
-/******************************************************************************/
+/** @} */
 
-/**
- * @defgroup PublicFunc Public Functions
- * @{
- */
-
-/**@}*/

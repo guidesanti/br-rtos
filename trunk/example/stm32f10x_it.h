c75fa018d135471574d719bb2bdb7e97226dbe7c
guilherme.desanti@gmail.com <guilherme.desanti@gmail.com@94cdb9b2-8835-1a89-94b0-4352372fe932>
2012-08-02 15:55:29 +0000

    Just commiting the folder before removing it.

--- trunk/example/stm32f10x_it.h
+++ trunk/example/stm32f10x_it.h
@@ -12,10 +12,10 @@ void HardFault_Handler(void);
 void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
-//void SVC_Handler(void);
+void SVC_Handler(void);
 void DebugMon_Handler(void);
-//void PendSV_Handler(void);
-//void SysTick_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
 
 #define WWDG_IRQHandler             (0U)
 #define PVD_IRQHandler              (0U)

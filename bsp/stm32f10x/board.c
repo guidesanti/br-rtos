8966e31e091919ac7912fc071e1913137b67f16e
guilherme.desanti <guilherme.desanti@gmail.com>
2012-08-02 01:52:25 +0000

    Added the bsp folder and initial structure to support the stm32f10x MCUs from ST Microelectronics.

---
+++ bsp/stm32f10x/board.c
@@ -0,0 +1,35 @@
+/**
+ * @file        board.c
+ * @date        2012, August 01
+ * @author      Guilherme A. de Santi <guidesanti@yahoo.com.br>
+ * @copyright   &copy; 2012, BR-RTOS Development Team
+ * @brief       TODO: add brief description
+ *
+ * This file is part of the BR-RTOS.
+ * The license distribution terms may found at COPYING or LICENSE file
+ * within the source directory tree.
+ */
+
+/**
+ * @addtogroup STM32F10xBSP STM32F10x BSP
+ * @{
+ */
+
+/******************************************************************************/
+/* I N C L U D E S                                                            */
+/******************************************************************************/
+
+
+/******************************************************************************/
+/* C O N S T A N T ,  M A C R O  A N D  T Y P E  D E F I N I T I O N S        */
+/******************************************************************************/
+
+/**
+ * @name Constants, Macro and Type Definitions
+ * @{
+ */
+
+/** @} */
+
+/** @} */
+
